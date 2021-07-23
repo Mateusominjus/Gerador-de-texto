c1263607029c1c2fea0e3c5ff079fde9047e44c0
Mateus <mateus.queiroz@omnijus.com.br>
2021-07-23 12:26:01 -0300

    att

---
+++ index.js
@@ -0,0 +1,7 @@
+React.createElement(
+    "h1",
+    null,
+    "teste"
+);
+
+//babel --presets react -index.jsx --out index.js
---
+++ index.jsx
@@ -0,0 +1,5 @@
+<h1>
+    teste
+</h1>
+
+//babel --presets react -index.jsx --out index.js
