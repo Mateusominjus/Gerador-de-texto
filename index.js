31976412d0c091d10cf73484f4f68f19e2f33739
Mateus <mateus.queiroz@omnijus.com.br>
2021-07-23 13:22:14 -0300

    rodando

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-React.createElement(
+let r = React.createElement(
     "h1",
     null,
     "teste"
 );
 
 //babel --presets react -index.jsx --out index.js
+
+ReactDOM.render(r, $("#root").get(0));
--- index.jsx
+++ index.jsx
@@ -1,5 +1,7 @@
-<h1>
+let r = <h1>
     teste
 </h1>
 
 //babel --presets react -index.jsx --out index.js
+
+ReactDOM.render(r,$("#root").get(0))
