c7d9fb3ef4c5c8a30118b5b6761e1c30c35ab301
mateusominjus <mateus.queiroz@ominjus.com.br>
2021-07-29 15:06:02 -0300

    instalador

--- install.py
+++ install.py
@@ -3,8 +3,10 @@
 
 
 import os
+from shutil import copyfile
 
-
+def gera_caminho(dado):
+    return f'{dado['caminho']}\\{dado['arquivo']}'
 
 def retorna_tipo(caminho:str):
         estensoes = {
@@ -17,6 +19,7 @@ def retorna_tipo(caminho:str):
             if arq in caminho:
                 return estensoes[arq]
 
+
 def copila_babel(caminho:str):
     nome_de_saida  = caminho.replace('jsx','js')
     comando = f'''babel --presets /usr/local/lib/node_modules/babel-preset-react 
@@ -24,20 +27,23 @@ def copila_babel(caminho:str):
     print(comando)
 
 def arquivos_do_app():
-    caminhos = []
+    dados = []
     for r, d, arquivos in os.walk('App'):
         for arq in arquivos:
-            caminhos.append(f'{r}\\{arq}')
-    return caminhos
+            dados.append({'caminho':r,'arquivo':arq})
+    return dados
+
 
 
 def main():
-    caminhos = arquivos_do_app()
-
-    for caminho in caminhos:
-        tipo = retorna_tipo(caminho)
-        print(tipo)
- 
+    dados = arquivos_do_app()
+    for dado in dados:
+        tipo =  retorna_tipo(dado['arquivo']
+        caminho = gera_caminho(dado)
+        if tipo == 'jsx':
+            copila_babel(caminho)
+        else:
+           copyfile(caminho, f'saida/{tipo}') 
 
 
 main()
