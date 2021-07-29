239c54fcc07a7664d82eda2a1b00647a7ece5239
mateusominjus <mateus.queiroz@ominjus.com.br>
2021-07-29 16:43:56 -0300

    att

--- install.py
+++ install.py
@@ -5,8 +5,11 @@
 import os
 from shutil import copyfile
 
-def gera_caminho(dado):
-    return f'{dado['caminho']}\\{dado['arquivo']}'
+def gera_caminho_de_entrada(dado):
+    return f'{ dado["caminho"]}\\{dado["arquivo"]}'
+
+def gera_caminho_de_saida(dado,tipo):
+    return f'saida\\{tipo}\\{dado["arquivo"]}'
 
 def retorna_tipo(caminho:str):
         estensoes = {
@@ -19,12 +22,11 @@ def retorna_tipo(caminho:str):
             if arq in caminho:
                 return estensoes[arq]
 
-
-def copila_babel(caminho:str):
-    nome_de_saida  = caminho.replace('jsx','js')
+def copila_babel(caminho_de_entrada,caminho_de_saida):
+    nome_de_saida  = caminho_de_saida.replace('jsx','js')
     comando = f'''babel --presets /usr/local/lib/node_modules/babel-preset-react 
-        App/{caminho} --out-file saida/scripts/{nome_de_saida}'''
-    print(comando)
+    {caminho_de_entrada} --out-file {caminho_de_saida}'''
+    os.system(comando)
 
 def arquivos_do_app():
     dados = []
@@ -33,18 +35,19 @@ def arquivos_do_app():
             dados.append({'caminho':r,'arquivo':arq})
     return dados
 
-
-
 def main():
     dados = arquivos_do_app()
     for dado in dados:
-        tipo =  retorna_tipo(dado['arquivo']
-        caminho = gera_caminho(dado)
-        if tipo == 'jsx':
-            copila_babel(caminho)
-        else:
-           copyfile(caminho, f'saida/{tipo}') 
+        tipo =  retorna_tipo(dado['arquivo'])
+        caminho_de_entrada = gera_caminho_de_entrada(dado)
+        caminho_de_saida = gera_caminho_de_saida(dado,tipo) 
 
+        if tipo == 'jsx':
+            caminho_de_saida = gera_caminho_de_saida(dado,"scripts") 
+            copila_babel(caminho_de_entrada,caminho_de_saida)
+        else:
+            copyfile(caminho_de_entrada, caminho_de_saida) 
+        
 
 main()
 
